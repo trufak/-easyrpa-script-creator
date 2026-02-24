2fca7c6b9f485a0a1fdcf08fec96ba480441e482
Trufakin Sergey <trufakin.sergey@mail.ru>
2026-02-24 11:49:45 +0700

    fix: повторно объединить в index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
 import fsPromises from "fs/promises";
 import path from "path";
-import {
-  ExternalDataNodeType,
-  ExternalDataEdgeType,
-  ExternalDataStateTerminalType,
-  HandleTypes,
-  NodeTypes,
-  ExternalDataHandleType,
-  MetaDataType,
-  VariablesNamesType,
-  EdgeSortedType,
-  CollectionType,
-} from "./types";
-import { getSubFlowName, normalize } from "./utils";
 
 export interface IScriptCreator {
   createScript(
@@ -689,3 +676,111 @@ export class ScriptCreator implements IScriptCreator {
     return resultArray.join("\n");
   }
 }
+
+//types
+export type CollectionType = {
+  name: string;
+  nodes: ExternalDataNodeType[];
+}
+
+export type ExternalDataNodeType = {
+  name: string;
+  label: { ru: string, en: string };
+  collection: string;
+  cid: string;
+  parentsId: string[];
+  type: NodeTypes;
+  path: string;
+  scriptPath: string;
+  id: string;
+  inputHandles: ExternalDataHandleType[];
+  outputHandles: ExternalDataHandleType[];
+  stateTerminals: ExternalDataStateTerminalType[];
+  connectedEdges: ExternalDataEdgeType[];
+  contentValues?: ContentValuesType,
+  children?: ExternalDataNodeType[];
+  dataSubFlow?: ExternalDataDefaultFlowType;
+  scriptLineNumber?: number;
+};
+
+export type ExternalDataDefaultFlowType = {
+  nodes: ExternalDataNodeType[];
+  edges: ExternalDataEdgeType[];
+};
+
+export type ContentValuesType = { [key: string]: string | number | { [key: string]: string | number } | (string | number)[]};
+
+export type ExternalDataEdgeType = {
+  target: string;
+  targetCid: string;
+  targetHandle?: string;
+  source: string;
+  sourceCid: string;
+  sourceHandle?: string;
+  isReplacement: boolean;
+};
+
+export type ExternalDataStateTerminalType = {
+  inputHandles: ExternalDataHandleType[];
+  outputHandles: ExternalDataHandleType[];
+  handlesType: HandleTypes;
+  cid: string;
+};
+
+export type ExternalDataHandleType = {
+  name: string;
+  hidden: boolean;
+  value: string;
+  connectedEdges: ExternalDataEdgeType[];
+  dataHandleSubFlowTerminal?: ExternalDataHandleType;
+  nodeСid: string;
+};
+
+export enum HandleTypes {
+  INPUT = 'input',
+  OUTPUT = 'output'
+}
+
+export enum NodeTypes {
+  CONTROL = 'control',
+  CONTAINER = 'container',
+  STATETERMINAL = 'stateTerminal',
+  SUBFLOW = 'subFlow',
+  EXTERNALSTATETERMINAL = 'externalStateTerminal'
+}
+
+export enum LanguageNames {
+  RU = 'ru',
+  EN = 'en'
+};
+
+export type VariablesNamesType = {
+  nodesVariableName: string;
+  containerDataName: string;
+  nodesJsonVariableName: string;
+  inputDictName: string;
+  inHandlesName: string;
+  defaultScriptName: string;
+}
+
+//тии мета-данных
+export type MetaDataType = {
+  appLanguage?: LanguageNames;
+};
+
+//типы для сортировки
+export type EdgeSortedType = ExternalDataEdgeType & { visited?: boolean };
+
+//utils
+//получение имени SubFLow
+export const getSubFlowName = (
+  subFlow: ExternalDataNodeType,
+): string => {
+  return `${subFlow.name}_${subFlow.cid}`;
+};
+
+export const normalize = (p: string): string => {
+  return path.normalize(
+    p.replaceAll('\\','/')
+  )
+};
